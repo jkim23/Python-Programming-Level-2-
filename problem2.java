594f52e51b3723cb9a6f4ad3fbe1f925f756f4db
jkim23 <47127753+jkim23@users.noreply.github.com>
2019-06-13 15:47:03 -0500

    module 5
    module 5 lab activity

---
+++ problem2.java
@@ -0,0 +1,12 @@
+// inclass problem 2 
+// JT KIM 
+
+class problem 2
+{
+	public static void main(String []args)
+	{
+		int sampleNumber = 5;
+		
+		System.out.println(sampleNumber);
+	}
+}
---
+++ problem3.java
@@ -0,0 +1,11 @@
+class problem 3	
+{
+	public static void main{String []args}
+	{
+		byte a = 12;
+		short b = 15;
+		int c = 20;
+		long d = 10000000000;
+		float f = 12.5f
+		double g = 12.5 
+		string h = "test"
